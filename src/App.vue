57d71e355e591d7da75d2831315b87f0c62fcfd9
hdinjos <hdinjos@gmail.com>
2020-12-23 13:24:11 +0700

    oke first

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>ini state {{number}} {{alamat}}</h1>
+    <HelloWorld msg="tentang alamat"/>
+    <p>{{getName}} itu name</p>
+    <p>{{people.age}} itu age</p>
+    <p>{{people.address}} itu address</p>
+    <p>{{numberIcr}}</p>
+    <button v-on:click="changeName">Ganti nama menjadi bubu</button>
+    <button @click="displayAlert">Display</button>
+    <p v-for="h in cars" v-bind:key="h">{{h}}</p>
+
+    <img v-if="image" alt="Vue logo" src="./assets/logo.png">
+    <img v-else alt="gambar 2" src="./assets/a.png">
+
   </div>
 </template>
 
@@ -12,6 +23,35 @@ export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  data: ()=> {
+    return {
+      number: 1,
+      alamat: 'jl. kaliurang no. 20',
+      people: {
+        name: 'Aisyah',
+        age: 20,
+        address: 'Yogja'
+      },
+      cars: ['CRV', 'JAZZ', 'MOBILIO'],
+      image: true
+    }
+  },
+  computed: {
+    getName(){
+      return this.people.name.toUpperCase();
+    },
+    numberIcr(){
+      return  'selamat datang';
+    }
+  },
+  methods: {
+    changeName(){
+      this.people.name = 'Bubu';
+    },
+    displayAlert(){
+      alert(this.number);
+    }
   }
 }
 </script>
